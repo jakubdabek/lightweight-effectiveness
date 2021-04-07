6782634f7facc4da07abb0ea6b4342e4cdf01910
Jakub Dąbek <jakub.dabek@gmail.com>
2021-04-07 16:58:01 +0200

    Add results volume for docker

--- run-docker.sh
+++ run-docker.sh
@@ -2,9 +2,11 @@ set -eux
 
 PROJECTS_VOLUME="pbr-lightweight-projects"
 m2_VOLUME="pbr-lightweight-m2"
+RESULTS_VOLUME="pbr-lightweight-results"
 image="pbr-lightweight"
 
 $DOCKER run --rm \
     -v "${PROJECTS_VOLUME}:/root/experiments/projects" \
     -v "${m2_VOLUME}:/root/.m2" \
+    -v "${RESULTS_VOLUME}:/root/experiments/results" \
     -it "$image";
