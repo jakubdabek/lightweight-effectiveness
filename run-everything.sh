c055049baccafdda353c13620918f64326286355
Jakub Dąbek <jakub.dabek@gmail.com>
2021-04-13 19:43:14 +0200

    Fix scripts for running experiments

--- run-everything.sh
+++ run-everything.sh
@@ -2,5 +2,5 @@ set -eux
 
 ./for-each-project.sh  # checkout all projects
 ./make-runner.sh  # generate script for running mutations
-./run_experiment-ALL.sh  # run mutations (by default with ALL operators)
+./run_experiment_ALL.sh  # run mutations (by default with ALL operators)
 ./run-no-mutations.sh
--- run-no-mutations.sh
+++ run-no-mutations.sh
@@ -7,4 +7,4 @@ python3 effectiveness/mutation/calculate_results.py  # parse PIT output
 echo "******************** Stage done *******************" && read -n1
 python3 effectiveness/metrics/aggregate_sources.py  # aggregate results of mutations and code metrics
 echo "******************** Stage done *******************" && read -n1
-python3 effectiveness/classification.py
+python3 effectiveness/classification.py '-ALL_median'
