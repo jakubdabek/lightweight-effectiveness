d966836091553c81422e99a07c6bf492d4083417
Jakub Dąbek <jakub.dabek@gmail.com>
2021-04-14 20:27:41 +0200

    Add default docker command

--- run-docker.sh
+++ run-docker.sh
@@ -7,7 +7,7 @@ m2_VOLUME="pbr-lightweight-m2"
 RESULTS_VOLUME="pbr-lightweight-results"
 image="pbr-lightweight"
 
-$DOCKER run --rm \
+"${DOCKER-docker}" run --rm \
     -v "${PROJECTS_VOLUME}:/root/experiments/projects" \
     -v "${m2_VOLUME}:/root/.m2" \
     -v "${RESULTS_VOLUME}:/root/experiments/results" \
