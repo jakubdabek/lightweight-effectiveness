d8cea09c786182fc721c6f28d537c4f1cb12335a
Jakub Dąbek <jakub.dabek@gmail.com>
2021-04-25 00:10:15 +0200

    Fix pandas operation

--- effectiveness/mutation/utils.py
+++ effectiveness/mutation/utils.py
@@ -5,4 +5,4 @@ import pandas as pd
 
 
 def get_projects(path: Path) -> List[str]:
-    return pd.read_csv(path)['project'].unique().str.split("/").str[1]
+    return pd.read_csv(path)['project'].str.split("/").str[1].unique().tolist()
